3e8b8afa82ddf869b388c91663974499288a6886
Alan Martin <alan.edward.martin@gmail.com>
2023-08-11 02:46:51 -0400

    add hook desc to useResizeHandler

--- src/hooks/useResizeHandler.ts
+++ src/hooks/useResizeHandler.ts
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 
 type ResizeCallback = () => void;
 
+/**
+ * @description A hook that adds a resize event listener to the window object.
+ */
 export default function useResizeHandler(onResize: ResizeCallback): void {
   useEffect(() => {
     const handleResize = () => {
