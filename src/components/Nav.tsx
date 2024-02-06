fe3b71a129dfb76f3ec71586b3ea76729a0f4965
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-02-06 16:48:07 -0500

    added focus trap

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -14,6 +14,7 @@ import { navbarContent } from '../content';
 import { getSlideFadeTransitionClasses } from '../styles/toggleAnimationClasses';
 import { TEST_IDS } from '../test/constants';
 import useWindowDimensions from '../hooks/useWindowDimensions';
+import FocusTrap from 'focus-trap-react';
 
 export default function Nav() {
   // State
@@ -47,58 +48,60 @@ export default function Nav() {
       )}`}
       ref={navRef}
     >
-      <div className="container mx-auto flex h-full w-full flex-col justify-between md:flex-row md:items-center">
-        <div className="flex items-center justify-between">
-          <a href="/" aria-label="home" data-testid="home-link">
-            <Logo
-              aria-hidden="true"
-              title="Pennylane Logo"
-              data-testid={TEST_IDS.PENNYLANE_WORDMARK_LOGO}
-            />
-          </a>
-          <button
-            aria-label={menuOpen ? 'close mobile menu' : 'open mobile menu'}
-            aria-expanded={menuOpen}
-            className="md:hidden"
-            onClick={() => setMenuOpen((prev) => !prev)}
-          >
-            <FaBars size={24} aria-hidden="true" />
-          </button>
-        </div>
+      <FocusTrap active={menuOpen}>
+        <div className="container mx-auto flex h-full w-full flex-col justify-between md:flex-row md:items-center">
+          <div className="flex items-center justify-between">
+            <a href="/" aria-label="home" data-testid="home-link">
+              <Logo
+                aria-hidden="true"
+                title="Pennylane Logo"
+                data-testid={TEST_IDS.PENNYLANE_WORDMARK_LOGO}
+              />
+            </a>
+            <button
+              aria-label={menuOpen ? 'close mobile menu' : 'open mobile menu'}
+              aria-expanded={menuOpen}
+              className="md:hidden"
+              onClick={() => setMenuOpen((prev) => !prev)}
+            >
+              <FaBars size={24} aria-hidden="true" />
+            </button>
+          </div>
 
-        <div
-          className={`flex flex-col gap-6 overflow-hidden opacity-100 md:flex-row md:items-center md:gap-12
+          <div
+            className={`flex flex-col gap-6 overflow-hidden opacity-100 md:flex-row md:items-center md:gap-12
           ${
             !menuOpen && windowWidth < 769
               ? 'invisible h-0 pt-0 opacity-0'
               : 'visible h-auto pt-6 opacity-100'
           }
           transition-all duration-200 md:h-auto md:overflow-y-auto md:pt-0`}
-        >
-          <ul className="flex flex-col gap-3 md:flex-row md:items-center md:gap-6">
-            {navbarContent.internalLinks.map(({ label, url }) => (
-              <li key={label}>
-                <NavLink url={url}>{label}</NavLink>
-              </li>
-            ))}
-          </ul>
+          >
+            <ul className="flex flex-col gap-3 md:flex-row md:items-center md:gap-6">
+              {navbarContent.internalLinks.map(({ label, url }) => (
+                <li key={label}>
+                  <NavLink url={url}>{label}</NavLink>
+                </li>
+              ))}
+            </ul>
 
-          <ul className="flex items-center gap-6">
-            {navbarContent.socialLinks.map(({ label, icon: Icon, url }) => (
-              <li key={label}>
-                <NavLink
-                  title={label}
-                  aria-label={label}
-                  url={url}
-                  openInNewTab
-                >
-                  <Icon size={24} aria-hidden="true" />
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+            <ul className="flex items-center gap-6">
+              {navbarContent.socialLinks.map(({ label, icon: Icon, url }) => (
+                <li key={label}>
+                  <NavLink
+                    title={label}
+                    aria-label={label}
+                    url={url}
+                    openInNewTab
+                  >
+                    <Icon size={24} aria-hidden="true" />
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
+      </FocusTrap>
     </nav>
   );
 }
